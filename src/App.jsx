fe8e63dcc5a642e849218eb064e8c465fee7c922
Soumya Prakash Datta <dattasoumyaprakash@gmail.com>
2023-11-20 00:08:04 +0800

    wip: ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
+import {Row, Col} from 'antd'
+
 import Chat from './Chat'
 import ChatContext from './ChatContext'
 
 function App() {
   return (
-    <div>
-      <ChatContext />
-      <Chat />
-    </div>
+    <Row style={{backgroundColor: "yellow"}}>
+      <Col span={18} style={{backgroundColor: "blue", height: "100vh"}}>
+        <Chat />
+      </Col>
+      <Col span={6} style={{backgroundColor: "green", height: "100vh"}}>
+        <ChatContext />
+      </Col>
+    </Row>
   )
 }
 
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,5 +1,22 @@
+import { Row, Col, Space, Layout } from 'antd'
 import React from 'react'
 import { useState } from 'react'
+const { Footer, Content } = Layout
+
+const contentStyle = {
+  textAlign: 'center',
+  height: 750,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+}
+
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 100,
+  backgroundColor: '#7dbcea',
+}
 
 function Chat() {
   const [message, setMessage] = useState('')
@@ -49,40 +66,53 @@ function Chat() {
   }
 
   return (
-    <div>
-      <div>
-        {chatList.map((chat, index) => {
-          return (
-            <div
-              key={index}
-              style={{
-                backgroundColor: chat.role == 'client' ? 'yellow' : 'cyan',
-              }}
-            >
-              <p>{chat.message}</p>
+    <Space
+      direction="vertical"
+      style={{ height: '100vh', width: '100%' }}
+      size={[0, 48]}
+    >
+      <Layout>
+        <Content style={contentStyle}>
+          <Row>
+            <Col>
+              {chatList.map((chat, index) => {
+                return (
+                  <div
+                    key={index}
+                    style={{
+                      backgroundColor:
+                        chat.role == 'client' ? 'yellow' : 'cyan',
+                    }}
+                  >
+                    <p>{chat.message}</p>
+                  </div>
+                )
+              })}
+            </Col>
+            {loading ? <div>loading data ... </div> : null}
+            {error ? <div>error: {JSON.stringify(error)}</div> : null}
+          </Row>
+        </Content>
+        <Footer style={footerStyle}>
+          <Col>
+            <input
+              type="text"
+              value={message}
+              placeholder="Type your message"
+              onChange={handleInputText}
+            />
+            <div>
+              <button
+                onClick={() => getChatCompletions()}
+                disabled={loading || message.trim().length == 0}
+              >
+                Send
+              </button>
             </div>
-          )
-        })}
-      </div>
-      <div>
-        <input
-          type="text"
-          value={message}
-          placeholder="Type your message"
-          onChange={handleInputText}
-        />
-        <div>
-          <button
-            onClick={() => getChatCompletions()}
-            disabled={loading || message.trim().length == 0}
-          >
-            Send
-          </button>
-        </div>
-      </div>
-      {loading ? <div>loading data ... </div> : null}
-      {error ? <div>error: {JSON.stringify(error)}</div> : null}
-    </div>
+          </Col>
+        </Footer>
+      </Layout>
+    </Space>
   )
 }
 
